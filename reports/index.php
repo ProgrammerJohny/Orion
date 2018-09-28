06a9265fe39a0da337e47f4c91e83563af1335a8
ProgrammerJohny <43511264+ProgrammerJohny@users.noreply.github.com>
2018-09-29 00:03:51 +0200

    dodanie oraz modyfikacja głównej strony modułu Raporty

---
+++ reports/index.php
@@ -0,0 +1,52 @@
+<?
+session_start();
+
+
+if(empty($_SESSION["zalogowany"]))$_SESSION["zalogowany"]=0;
+
+mysql_connect("localhost", "admin", "Webmaster2017")or die("Nie można nawiązać połączenia z bazą");
+mysql_select_db("aplikacja")or die("Wystąpił błąd podczas wybierania bazy danych");
+
+function ShowLogin($komunikat=""){
+	echo "$komunikat<br>";
+	echo "<form action='../../main.php' method=post>";
+	echo "Login <input type=text  name=login><br>";
+	echo "Hasło: <input type=password   name=haslo><br>";
+	echo "<button type=submit value='Zaloguj!' id='submitLogin'>Zaloguj!</button>";
+	echo "</form>";
+
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+	<script src="../../scripts/js/main.js"></script>
+	<title>ORION - Archiwum - Strona Główna - Klienci Indywidualni - Umowy</title>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" type="text/css" href="../../css/style.css" />
+</head>
+<body>
+<div class="container-fluid">
+<div class="card">
+  <div class="card-body">
+    <a href="#" role="button" class="btn btn-info">Przejdź do raportów dla Klientów Indywidualnych</a>
+  </div>
+</div>
+<div class="card">
+  <div class="card-body">
+    <a href="#" role="button" class="btn btn-info">Przejdź do raportów dla Klientów Biznesowych</a>
+  </div>
+</div>
+</div>
+<button class=" btn btn-link"><a href="../../main.php">Powrót do strony głównej serwisu</a></button>
+</body>
+</html>
