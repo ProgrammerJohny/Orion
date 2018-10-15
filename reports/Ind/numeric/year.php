1cdf5e7bdcc5527364cf2b7c4ae653e5cd75f7a8
ProgrammerJohny <43511264+ProgrammerJohny@users.noreply.github.com>
2018-10-15 19:00:37 +0200

    Update year.php

--- reports/Ind/numeric/year.php
+++ reports/Ind/numeric/year.php
@@ -1,18 +1,69 @@
+<meta charset="utf-8">
 <?
-require_once('../../PHPExcel-1.8/Classes/PHPExcel.php');
+try {
+  $conn = new PDO('mysql:host=localhost;dbname=aplikacja', 'admin', 'Webmaster2017');
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->query("set names utf8");
+  $stmt = $conn -> query('SELECT * FROM customersInd');
+  $headers = array(
+  'ID_cus_ind' =>'ID',
+  'name_cus_ind'=>'Imię',
+  'surname_cus_ind' =>'Nazwisko',
+  'pesel_cus_ind'=>'PESEL',
+  'number_cus_ind'=>'Numer Klienta',
+  'tel_cus_ind'=>'Telefon',
+  'email_cus_ind'=>'E-mail',
+  'country_reg_cus_ind'=>'Kraj - zameldowanie',
+  'code_reg_cus_ind'=>'Kod pocztowy - zameldowanie',
+  'city_reg_cus_ind'=>'Miejscowość - zameldowanie',
+  'street_reg_cus_ind'=>'Ulica - zameldowanie',
+  'numberhouse_reg_cus_ind'=>'Numer budynku - zameldowanie',
+  'numberflat_reg_cus_ind'=>'Numer lokalu - zameldowanie',
+  'country_corr_cus_ind'=>'Kraj - zamieszkanie',
+  'code_corr_cus_ind'=>'Kod pocztowy - zamieszkanie',
+  'city_corr_cus_ind'=>'Miejscowość - zamieszkanie',
+  'street_corr_cus_ind'=>'Ulica - zamieszkanie',
+  'numberhouse_corr_cus_ind'=>'Numer budynku - zamieszkanie',
+  'numberflat_corr_cus_ind'=>'Numer lokalu - zamieszkanie',
+  'period_cus_ind'=>'Data podpisania umowy',
+  'contract_status_cus_ind'=>'Aktualny status umowy');
+  $data = date('d.m.Y H:i:s');
+$raport = fopen("Raport roczny ilościowy -".date('d.m.Y').' - '.date('d.m.Y',strtotime("-1 year"))."- Klienci Indywidualni.xls","w");
 
-$PHPEXcel = new PHPExcel();
+fwrite($raport,"Raport wygenerowano:".date('d.m.Y H:i:s')."\n");
+foreach ($headers as $key ) {
+  echo "<b>";
+  fwrite ($raport,$key."\t");
+  echo "</b>";
+}
+while($row = $stmt->fetch()) {
+  fwrite($raport,"\n");
+  fwrite($raport,$row['ID_cus_ind']."\t");
+  fwrite($raport,$row['name_cus_ind']."\t");
+  fwrite($raport,$row['surname_cus_ind']."\t");
+  fwrite($raport,$row['pesel_cus_ind']."\t");
+  fwrite($raport,$row['number_cus_ind']."\t");
+  fwrite($raport,$row['tel_cus_ind']."\t");
+  fwrite($raport,$row['email_cus_ind']."\t");
+  fwrite($raport,$row['country_reg_cus_ind']."\t");
+  fwrite($raport,$row['code_reg_cus_ind']."\t");
+  fwrite($raport,$row['city_reg_cus_ind']."\t");
+  fwrite($raport,$row['street_reg_cus_ind']."\t");
+  fwrite($raport,$row['numberhouse_reg_cus_ind']."\t");
+  fwrite($raport,$row['numberflat_reg_cus_ind']."\t");
+  fwrite($raport,$row['country_corr_cus_ind']."\t");
+  fwrite($raport,$row['code_corr_cus_ind']."\t");
+  fwrite($raport,$row['city_corr_cus_ind']."\t");
+  fwrite($raport,$row['street_corr_cus_ind']."\t");
+  fwrite($raport,$row['numberhouse_corr_cus_ind']."\t");
+  fwrite($raport,$row['numberflat_corr_cus_ind']."\t");
+  fwrite($raport,$row['period_cus_ind']."\t");
+  fwrite($raport,$row['contract_status_cus_ind']."\t");
 
-$PHPExcel->getProperties()->setCreator('ORION');
-$PHPExcel->getProperties()->setLastModifiedBy('Jan Zalesiński');
-$PHPExcel->getProperties()->setTitle('Raport finansowy za ostatni rok');
-$PHPExcel->getProperties()->setSubject('Sprawozdanie za trzeci kwartał 2018');
-$PHPExcel->getProperties()->setDescription('Dużo zarobiliśmy, zarobimy jeszcze więcej');
-$PHPExcel->getProperties()->setKeywords('raport zestawienie finanse');
-$PHPExcel->getProperties()->setCategory('Finanse');
-$PHPExcel->getActiveSheet()->setCellValue('A1', 'zawartość komórki');
-$writer = new PHPExcel_Writer_Excel2007($PHPExcel);
-$writer->save('raport finansowy.xls');
-
-
+}
+echo "Raport wygenerowany!!<br><a href='index.php'>Powrót</a>";
+}
+catch(PDOException $e) {
+  echo "Błąd !!".$e->getMessage();
+}
 ?>
