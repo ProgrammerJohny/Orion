526cf4959d564fb6a52de58b7e54aca3dbfe6c30
ProgrammerJohny <43511264+ProgrammerJohny@users.noreply.github.com>
2018-10-19 19:49:17 +0200

    tworzenie raportowania z bazy do pdf

--- reports/Ind/numeric/year.php
+++ reports/Ind/numeric/year.php
@@ -1,74 +1,90 @@
-<meta charset="utf-8">
-<?
-try {
-  $conn = new PDO('mysql:host=localhost;dbname=aplikacja', 'admin', 'Webmaster2017');
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $conn->query("set names utf8");
-    $data = date('d.m.Y H:i:s');
-    $data1 = date('d.m.Y');
-    $data2 = date('d.m.Y',strtotime("-1 year"));
-  $stmt = $conn -> query("SELECT * FROM customersInd ");
-  $headers = array(
-  'ID_cus_ind' =>'ID',
-  'name_cus_ind'=>'Imię',
-  'surname_cus_ind' =>'Nazwisko',
-  'pesel_cus_ind'=>'PESEL',
-  'number_cus_ind'=>'Numer Klienta',
-  'tel_cus_ind'=>'Telefon',
-  'email_cus_ind'=>'E-mail',
-  'country_reg_cus_ind'=>'Kraj - zameldowanie',
-  'code_reg_cus_ind'=>'Kod pocztowy - zameldowanie',
-  'city_reg_cus_ind'=>'Miejscowość - zameldowanie',
-  'street_reg_cus_ind'=>'Ulica - zameldowanie',
-  'numberhouse_reg_cus_ind'=>'Numer budynku - zameldowanie',
-  'numberflat_reg_cus_ind'=>'Numer lokalu - zameldowanie',
-  'country_corr_cus_ind'=>'Kraj - zamieszkanie',
-  'code_corr_cus_ind'=>'Kod pocztowy - zamieszkanie',
-  'city_corr_cus_ind'=>'Miejscowość - zamieszkanie',
-  'street_corr_cus_ind'=>'Ulica - zamieszkanie',
-  'numberhouse_corr_cus_ind'=>'Numer budynku - zamieszkanie',
-  'numberflat_corr_cus_ind'=>'Numer lokalu - zamieszkanie',
-  'period_cus_ind'=>'Data podpisania umowy',
-  'contract_status_cus_ind'=>'Aktualny status umowy');
+<?php
+//PDF USING MULTIPLE PAGES
+//CREATED BY: Carlos Vasquez S.
+//E-MAIL: cvasquez@cvs.cl
+//CVS TECNOLOGIA E INNOVACION
+//SANTIAGO, CHILE
 
-$raport = fopen("Raport roczny ilościowy -".date('d.m.Y').' - '.date('d.m.Y',strtotime("-1 year"))."- Klienci Indywidualni.xls","w");
+require('../../fpdf/fpdf.php');
 
-fwrite($raport,"Raport wygenerowano:".date('d.m.Y H:i:s')."\n");
-fwrite($raport,"Jeżeli status umowy = 0, oznacza to, że jest ona Aktywna\n");
-foreach ($headers as $key ) {
-  fwrite ($raport,$key."\t");
+//Connect to your database
+$conn = new PDO('mysql:host=localhost;dbname=aplikacja', 'admin', 'Webmaster2017');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->query("set names utf8");
+//$stmt = $conn -> query('SELECT * FROM customersInd');
+//Create new pdf file
+$pdf=new FPDF();
+
+//Disable automatic page break
+$pdf->SetAutoPageBreak(false);
+
+//Add first page
+$pdf->AddPage();
+
+//set initial y axis position per page
+$y_axis_initial = 25;
+
+//print column titles
+$pdf->SetFillColor(232,232,232);
+$pdf->SetFont('Arial','B',12);
+$pdf->SetY($y_axis_initial);
+$pdf->SetX(25);
+$pdf->Cell(30,6,'ID',1,0,'L',1);
+$pdf->Cell(100,6,'Imię',1,0,'L',1);
+$pdf->Cell(30,6,'Nazwisko',1,0,'R',1);
+
+$y_axis = $y_axis + $row_height;
+
+//Select the Products you want to show in your PDF file
+$result=mysql_query('SELECT ID_cus_ind, name_cus_ind,surname_cus_ind FROM customersInd',$link);
+
+//initialize counter
+$i = 0;
+
+//Set maximum rows per page
+$max = 25;
+
+//Set Row Height
+$row_height = 6;
+
+while($row = mysql_fetch_array($result))
+{
+    //If the current row is the last one, create new page and print column title
+    if ($i == $max)
+    {
+        $pdf->AddPage();
+
+        //print column titles for the current page
+        $pdf->SetY($y_axis_initial);
+        $pdf->SetX(25);
+        $pdf->Cell(30,6,'CODE',1,0,'L',1);
+        $pdf->Cell(100,6,'NAME',1,0,'L',1);
+        $pdf->Cell(30,6,'PRICE',1,0,'R',1);
+
+        //Go to next row
+        $y_axis = $y_axis + $row_height;
+
+        //Set $i variable to 0 (first row)
+        $i = 0;
+    }
+
+    $code = $row['Code'];
+    $price = $row['Price'];
+    $name = $row['Code'];
+
+    $pdf->SetY($y_axis);
+    $pdf->SetX(25);
+    $pdf->Cell(30,6,$code,1,0,'L',1);
+    $pdf->Cell(100,6,$name,1,0,'L',1);
+    $pdf->Cell(30,6,$price,1,0,'R',1);
+
+    //Go to next row
+    $y_axis = $y_axis + $row_height;
+    $i = $i + 1;
 }
-while($row = $stmt->fetch()) {
-  fwrite($raport,"\n");
 
-  fwrite($raport,$row['ID_cus_ind']."\t");
-  fwrite($raport,$row['name_cus_ind']."\t");
-  fwrite($raport,$row['surname_cus_ind']."\t");
-  fwrite($raport,$row['pesel_cus_ind']."\t");
-  fwrite($raport,$row['number_cus_ind']."\t");
-  fwrite($raport,$row['tel_cus_ind']."\t");
-  fwrite($raport,$row['email_cus_ind']."\t");
-  fwrite($raport,$row['country_reg_cus_ind']."\t");
-  fwrite($raport,$row['code_reg_cus_ind']."\t");
-  fwrite($raport,$row['city_reg_cus_ind']."\t");
-  fwrite($raport,$row['street_reg_cus_ind']."\t");
-  fwrite($raport,$row['numberhouse_reg_cus_ind']."\t");
-  fwrite($raport,$row['numberflat_reg_cus_ind']."\t");
-  fwrite($raport,$row['country_corr_cus_ind']."\t");
-  fwrite($raport,$row['code_corr_cus_ind']."\t");
-  fwrite($raport,$row['city_corr_cus_ind']."\t");
-  fwrite($raport,$row['street_corr_cus_ind']."\t");
-  fwrite($raport,$row['numberhouse_corr_cus_ind']."\t");
-  fwrite($raport,$row['numberflat_corr_cus_ind']."\t");
-  fwrite($raport,$row['period_cus_ind']."\t");
+mysql_close($link);
 
-  fwrite($raport,$row['contract_status_cus_ind']."\t");
-
-}
-echo 'Raport wygenerowany!!<br>
-<a href="javascript:window.close()">Zamknij stronę</a>';
-}
-catch(PDOException $e) {
-  echo "Błąd !!".$e->getMessage();
-}
+//Send file
+$pdf->Output();
 ?>
